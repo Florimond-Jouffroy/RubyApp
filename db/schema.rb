c0f4508ed54e43c80c6dee0f057a8271da6c6738
Florimond Jouffroy <florimond.jouffroy@gmail.com>
2023-07-22 18:27:17 +0200

    mysql

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_19_203325) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "articles", force: :cascade do |t|
+  create_table "articles", charset: "utf8mb3", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
